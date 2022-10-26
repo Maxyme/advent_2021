1deee1c72e9a946710feb4b4fc8fc71e8d94aa9b
maxime <maxyme@outlook.com>
2022-10-25 20:16:05 -0400

    Final day 12 commit, only return counts instead of path to process faster.

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -4,48 +4,51 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 
-fn get_paths_from_node(curr_path: &[String], all_paths: &mut Vec<Vec<String>>, edges_map: &HashMap<&str, Vec<&str>>, can_visit_twice: bool, visit_counts: &mut HashMap<String, u32>) -> Vec<Vec<String>> {
+fn get_path_count(
+    curr_path: &[&str],
+    path_count: &mut usize,
+    edges_map: &HashMap<&str, Vec<&str>>,
+    can_visit_twice: bool,
+) -> usize {
+    // Get the last node in the current path to find the next targets in the path
+    let source = curr_path.last().unwrap();
+    let targets = edges_map.get(source).unwrap();
 
-    let source: &str = curr_path.last().unwrap().as_ref();
-    let targets: &Vec<&str> = edges_map.get(source).unwrap();
     for target in targets {
-        let cave_name_is_lowercase = target.chars().all(|c| c.is_lowercase());
         if *target == "end" {
-            let mut new_path = curr_path.to_owned();
-            new_path.push("end".to_string());
-            all_paths.push(new_path)
-        }
-        else if cave_name_is_lowercase && curr_path.contains(&target.to_string()) {
-            // small cave has already been seen
-            if !can_visit_twice {
-                // Skip path
+            // Increase path count when reaching the end
+            *path_count += 1;
+        } else if target.chars().all(|c| c.is_lowercase()) && curr_path.contains(target) {
+            // Check if cave name is lowercase (small cave) and has already been seen
+            if can_visit_twice {
+                let mut new_path = curr_path.to_owned();
+                new_path.push(target);
+                // Set visit twice to false for the next small caves on this path
+                get_path_count(&new_path, path_count, edges_map, false);
+            } else {
+                // Skip path as we cannot visit a small cave twice
                 continue;
             }
-            // Add cave to registry of already visited once if no small cave is already visited
-            visit_count = visit_counts.get(&target).unwrap();
-            if visit_count == 0 {
-
-            }
-
         } else {
-            // Keep going further
+            // Keep going further with this path
             let mut new_path = curr_path.to_owned();
-            new_path.push(target.to_string());
-            get_paths_from_node(&new_path, all_paths, edges_map, can_visit_twice, visit_counts);
+            new_path.push(target);
+            get_path_count(&new_path, path_count, edges_map, can_visit_twice);
         }
     }
-    all_paths.to_vec()
+    *path_count
 }
 
 fn main() {
-    let input = fs::read_to_string("example.txt").expect("Error reading file.");
+    let input = fs::read_to_string("input.txt").expect("Error reading file.");
 
-    // Parse edges and add to dict of source <-> targets
+    // Parse edges
     let edges: Vec<(&str, &str)> = input
         .lines()
         .map(|x| x.splitn(2, '-').collect_tuple().unwrap())
         .collect();
 
+    // Create dict of source <-> targets
     let mut edges_map: HashMap<&str, Vec<&str>> = HashMap::new();
     for (node_1, node_2) in edges {
         // Note: "start" can only be a key and "end" cannot be a key
@@ -58,15 +61,10 @@ fn main() {
     }
 
     // Part 1: small caves can only be visited once
-    let mut all_paths: Vec<Vec<String>> = Vec::new();
-    let start_path = ["start".to_string()].to_vec();
-    let mut visit_count = HashMap::new();
-    get_paths_from_node(&start_path, &mut all_paths, &edges_map, false, &mut visit_count);
-    println!("Part 1: All paths count: {:?}", all_paths.len());
+    let path_count = get_path_count(&["start"], &mut 0, &edges_map, false);
+    println!("Part 1: All paths count: {:?}", path_count);
 
     // Part 2: a single small cave may be visited twice
-    let mut all_paths_part_2: Vec<Vec<String>> = Vec::new();
-    let mut visit_count = HashMap::new();
-    get_paths_from_node(&start_path, &mut all_paths_part_2, &edges_map, true, &mut visit_count);
-    println!("Part 2: paths count: {:?}", all_paths_part_2.len());
+    let path_count = get_path_count(&["start"], &mut 0, &edges_map, true);
+    println!("Part 2: paths count: {:?}", path_count);
 }
