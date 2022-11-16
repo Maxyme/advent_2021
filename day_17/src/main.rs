88a8a0d4008b4a973d9370ce112ddcef51714456
maxime <maxyme@outlook.com>
2022-11-16 11:27:18 -0500

    Day 17

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,8 +1,26 @@
 use itertools::Itertools;
-use std::cmp::max;
 use std::fs;
 
 use std::ops::AddAssign;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Box {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+impl Box {
+    fn contains(&self, point: &Point) -> bool {
+        if point.x <= self.bottom_right.x
+            && point.x >= self.top_left.x
+            && point.y <= self.top_left.y
+            && point.y >= self.bottom_right.y
+        {
+            return true;
+        }
+        false
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
@@ -19,8 +37,43 @@ impl AddAssign for Point {
     }
 }
 
+// Find the top y from the initial_position
+fn top_y(initial_position: Point, initial_velocity: Point, target: &Box) -> Option<i32> {
+    let mut top_y = i32::MIN;
+    let mut velocity = initial_velocity;
+    let mut probe_position = initial_position;
+    loop {
+        // Update max_y
+        if probe_position.y > top_y {
+            top_y = probe_position.y;
+        }
+
+        // Update probe position
+        probe_position += velocity;
+
+        // Check if in target area
+        if target.contains(&probe_position) {
+            return Some(top_y);
+        }
+
+        // Otherwise check if passed target area
+        if probe_position.x > target.bottom_right.x || probe_position.y < target.bottom_right.y {
+            return None;
+        }
+
+        // Update velocity
+        // Add drag
+        if velocity.x > 0 {
+            velocity.x -= 1;
+        }
+
+        // Add gravity
+        velocity.y -= 1;
+    }
+}
+
 fn main() {
-    let input = fs::read_to_string("example.txt").expect("Error reading file");
+    let input = fs::read_to_string("input.txt").expect("Error reading file");
     let (x_coords, y_coords) = input
         .strip_prefix("target area: ")
         .unwrap()
@@ -35,6 +88,7 @@ fn main() {
         .map(|x| x.parse::<i32>().unwrap())
         .collect_tuple()
         .unwrap();
+
     let (min_y, max_y) = y_coords
         .strip_prefix("y=")
         .unwrap()
@@ -43,59 +97,53 @@ fn main() {
         .collect_tuple()
         .unwrap();
 
-    // Loop steps
-    // Note velocity must be an integer
-    let mut initial_velocity = Point { x: 6, y: 3 };
-    let mut probe_position = Point { x: 0, y: 0 };
-    let mut top_y = i32::MIN;
+    let target = Box {
+        top_left: Point { x: min_x, y: max_y },
+        bottom_right: Point { x: max_x, y: min_y },
+    };
 
-    // loop all possible initial velocities
-    'outer: loop {
-        let mut velocity = initial_velocity;
+    let probe_position = Point { x: 0, y: 0 };
 
-        'inner: loop {
-            println!("{:?}", probe_position);
-
-            // Update max_y
-            if probe_position.y > top_y {
-                top_y = probe_position.y;
-            }
-
-            // Update probe position
-            probe_position += velocity;
-
-            // Check if in target area
-            if probe_position.x <= max_x
-                && probe_position.x >= min_x
-                && probe_position.y <= max_y
-                && probe_position.y >= min_y
-            {
-                println!("Hit");
-                break;
-            }
-
-            // Otherwise check if passed target area
-            if probe_position.x > max_x || probe_position.y < min_y {
-                println!("Impossible");
-                break;
-            }
-
-            // Update velocity
-            // Add drag
-            if velocity.x > 0 {
-                velocity.x = velocity.x - 1;
-            }
-
-            // Add gravity
-            velocity.y -= 1;
+    let mut top: i32 = i32::MIN;
+    'outer: for x in 1..max_x {
+        for y in min_y..min_y.abs() {
+            let initial_velocity = Point { x, y };
+            // break when top doesn't
+            match top_y(probe_position, initial_velocity, &target) {
+                None => {
+                    continue;
+                }
+                Some(top_y) => {
+                    // Assume that top y is always increasing
+                    if top_y > top {
+                        top = top_y;
+                    } else {
+                        break 'outer;
+                    }
+                }
+            };
         }
     }
 
     // Part 1: Find the initial velocity that causes the probe to reach the
     // highest y position and still eventually be within the target area after any step.
     // What is the highest y position it reaches on this trajectory?
-    println!("{}", top_y)
+    println!("{}", top);
 
-    // Todo, make an ellipse from each point in the target area
-    // to the source and check what the max amplitude could be
+    // Part 2: Find every initial velocity that will hit the target
+    let mut count: usize = 0;
+    for x in 1..=max_x {
+        for y in min_y..min_y.abs() {
+            let initial_velocity = Point { x, y };
+            match top_y(probe_position, initial_velocity, &target) {
+                None => {
+                    continue;
+                }
+                Some(_) => {
+                    count += 1;
+                }
+            };
+        }
+    }
+    println!("{}", count);
 }
